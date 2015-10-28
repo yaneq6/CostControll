977c0ec250d5a3cb01781a4ce7c3831a2d17fbb7
Vladzor <krzysztof.niepokojczycki@gmail.com>
2015-10-28 22:12:32 +0100

    ???

--- db/migrate/20151026153011_market_product_shopping_session.rb
+++
@@ -1,8 +0,0 @@
-class MarketProductShoppingSession < ActiveRecord::Migration
-  def change
-    create_table :market_products_shopping_sessions, id: false do |t|
-      t.belongs_to :market_products, index: true
-      t.belongs_to :shopping_session, index: true
-    end
-  end
-end
